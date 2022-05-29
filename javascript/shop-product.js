d151663802e7c0ab40d6edaa8a64b2ba739e7944
Giorgia Meton <gior0166@stud.kea.dk>
2022-05-29 18:03:06 +0200

    applying burger menu to all pages

---
+++ javascript/shop-product.js
@@ -0,0 +1,12 @@
+//-------BURGER MENU-------
+document.querySelector(".menu-icon").addEventListener("click", showBRGMenu);
+
+function showBRGMenu() {
+  document.querySelector(".burger-menu-popUp").classList.remove("hide");
+
+  document.querySelector(".cross-icon").addEventListener("click", hideBRGMenu);
+}
+
+function hideBRGMenu() {
+  document.querySelector(".burger-menu-popUp").classList.add("hide");
+}
