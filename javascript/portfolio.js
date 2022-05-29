96a4c73af43def5e81860db868f2dd9517522c55
Giorgia Meton <gior0166@stud.kea.dk>
2022-05-29 17:39:33 +0200

    navigation mobile burger menu js functions

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,3 +1,16 @@
+//-------BURGER MENU-------
+document.querySelector(".menu-icon").addEventListener("click", showBRGMenu);
+
+function showBRGMenu() {
+  document.querySelector(".burger-menu-popUp").classList.remove("hide");
+
+  document.querySelector(".cross-icon").addEventListener("click", hideBRGMenu);
+}
+
+function hideBRGMenu() {
+  document.querySelector(".burger-menu-popUp").classList.add("hide");
+}
+
 const url = "https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product";
 
 fetch(url)
