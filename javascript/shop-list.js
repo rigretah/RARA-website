5ef3f7e27179025c395323ec1c76f02cb31c4751
Giorgia Meton <gior0166@stud.kea.dk>
2022-05-29 11:30:07 +0200

    local navigation mobile drop down js functions

---
+++ javascript/shop-list.js
@@ -0,0 +1,12 @@
+//LOCAL NAVIGATION DROP DOWN
+document.querySelector(".drop-down-arrow").addEventListener("click", dropDown);
+
+function dropDown() {
+  document.querySelector(".categories-drop-down").classList.remove("hide");
+
+  document.querySelector(".up-arrow").addEventListener("click", backUp);
+}
+
+function backUp() {
+  document.querySelector(".categories-drop-down").classList.add("hide");
+}
