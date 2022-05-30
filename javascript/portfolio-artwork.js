99bb5b3610cf60c962974c81ff2775cfd8f70d90
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-05-30 09:15:14 +0200

    portfolio artwork fetching id

--- javascript/portfolio-artwork.js
+++ javascript/portfolio-artwork.js
@@ -1,11 +1,17 @@
-// COPIED CODE - TEST
+// ALL ARTWORKS LINK: https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product?per_page=70
+// URL FOR WHEN LIST PAGES ARE SET UP
+
 // const urlParams = new URLSearchParams(window.location.search);
 // const id = urlParams.get("id");
-// const url = "https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product" + id;
 
-const url = "https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product/155";
+// const url = `https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product/${id}_embed`;
 
-// ALL ARTWORKS LINK: https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product?per_page=70
+// -----------------
+
+// URL FOR TESTING
+
+const url =
+  "https://cloudmae.dk/rara/wp_SEM2-EXAM/wp-json/wp/v2/product/155?_embed";
 
 fetch(url)
   .then((res) => res.json())
@@ -20,6 +26,13 @@ function displayArtwork(artwork) {
   const copy = template.cloneNode(true);
   // change content
   copy.querySelector(".artwork-title").textContent = artwork.title.rendered;
+  copy.querySelector(".artwork-category").textContent =
+    artwork._embedded["wp:term"][0][0].name;
+  copy.querySelector(".artwork-description").textContent = artwork.description;
+  copy.querySelector(
+    ".artwork-image"
+  ).src = `${artwork._embedded["wp:featuredmedia"][0].source_url}`;
+  copy.querySelector(".artwork-image").alt = artwork.title.rendered;
 
   // select parent
   const parent = document.querySelector("main");
